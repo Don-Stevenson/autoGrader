efff9e8217668c4ad4a9355a6d1fe0b412774a04
Don Stevenson <dstevenson@tribalscale.com>
2022-08-01 13:38:30 -0400

    add randm to autogrador js

--- autoGrader.js
+++ autoGrader.js
@@ -8,13 +8,26 @@ const validateNum = value => {
   } else return true
 }
 
-const randomGradeGenerator = value => {
-  let r = 0
-  for (let i = value; i > 0; i--) {
-    r += Math.random()
-  }
-  return `${((r / value) * 100).toFixed(1)}%`
+const randn_bm =()=> {
+
+  let u = 0, v = 0;
+  while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+  while(v === 0) v = Math.random();
+  let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+  num = num / 10.0 + 0.5; // Translate to 0 -> 1
+  if (num > 1 || num < 0) return randn_bm() // resample between 0 and 1
+  if (num < .20) return `<20 ${(num * 100+25).toFixed(1)}%`
+
+  // Skew to avoid complaints of low grades
+  if (num > .2 && num <= .45 ) return `>20 <45 ${(num * 100+20).toFixed(1)}%`
+  if (num > .45 && num <= .64 ) return `>45 <64 ${(num * 100+15).toFixed(1)}%`
+  if (num > .65 && num <= .75 ) return `>65 <75 ${(num * 100+10).toFixed(1)}%`
+  if (num > .75 && num <= .85 ) return `>75 <85 ${(num * 100+5).toFixed(1)}%`
+  if (num > .85 && num <= .98 ) return `>85 <98 ${(num * 100+2).toFixed(1)}%`
 }
+
+
+
 
 ;(async () => {
   const response = await prompts({
@@ -33,17 +46,12 @@ const randomGradeGenerator = value => {
   
     First, how many papers do you need to mark?`,
     validate: noOfPapers => validateNum(noOfPapers),
-
-    // for (let i = 0; index < noOfPapers; index++) {
-    //   const element = array[index];
-
-    // }
   })
 
   const finalGradesGenerator = () => {
     let finalGradesObj = {}
     for (let i = 1; i <= response.noOfPapers; i++) {
-      finalGradesObj[i]= randomGradeGenerator(i) 
+      finalGradesObj[i]= randn_bm()
         }
     return finalGradesObj
   
