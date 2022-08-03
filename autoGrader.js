d79dd4661285c8a09329be83ea6a74af7606f977
Don Stevenson <dstevenson@tribalscale.com>
2022-08-03 10:23:50 -0400

    refactor questions, add min, max and skew for random distribution

--- autoGrader.js
+++ autoGrader.js
@@ -8,54 +8,87 @@ const validateNum = value => {
   } else return true
 }
 
-const randn_bm = () => {
+function randn_bm(min, max, skew) {
   let u = 0,
     v = 0
   while (u === 0) u = Math.random() //Converting [0,1) to (0,1)
   while (v === 0) v = Math.random()
   let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+
   num = num / 10.0 + 0.5 // Translate to 0 -> 1
-  if (num > 1 || num < 0) return randn_bm() // resample between 0 and 1
-  if (!num) return randn_bm()
-  // Skew to avoid complaints of low grades
-  if (num < 0.2) return `${(num * 100 + 49).toFixed(1)}%`
-  if (num > 0.2 && num <= 0.64) return `${(num * 100 + 31).toFixed(1)}%`
-  if (num > 0.65 && num <= 0.75) return `${(num * 100 + 15).toFixed(1)}%`
-  if (num > 0.75 && num <= 0.85) return `${(num * 100 + 10).toFixed(1)}%`
-  if (num > 0.85 && num <= 0.98) return `${(num * 100 + 2).toFixed(1)}%`
+  if (num > 1 || num < 0)
+    num = randn_bm(min, max, skew) // resample between 0 and 1 if out of range
+  else {
+    num = Math.pow(num, skew) // Skew
+    num *= max - min // Stretch to fill range
+    num += min // offset to min
+  }
+  return `${num.toFixed(1)}%`
 }
 
-;(async () => {
-  const response = await prompts({
+const questions = [
+  {
     type: "number",
     name: "noOfPapers",
     initial: `number between 1 and 200`,
     message: `
-    * Welcome to AutoGrader! *
-    **************************
-    
-    A program to help you mark when you're feeling like,
+* Welcome to AutoGrader! *
+**************************
 
-    (┛ಠ_ಠ)┛ 彡 ┻━┻
+A program to help you mark when you're feeling like,
 
-    "fuck it, Imma throw these papers down the stairs
-    and assign marks based on where they land", 
-    but wisely remember, these papers are digital 
-    and I still need my laptop
-    
-    Note: Autograder marks on a bell curve. 
-    It progressively skews lower grades higher
-    to help avoid crying students during office hours. 
-    
-  
-    So, how many papers do you need to mark?`,
+(┛ಠ_ಠ)┛ 彡 ┻━┻
+
+"fuck it, Imma throw these papers down the stairs
+and assign marks based on where they land", 
+but wisely remember, these papers are digital 
+and I still need my laptop
+
+Note: Autograder marks on a bell curve. 
+It can progressively skew lower grades higher
+to help avoid crying students during office hours. 
+
+
+So, how many papers do you need to mark?`,
     validate: noOfPapers => validateNum(noOfPapers),
-  })
+  },
+  {
+    type: "number",
+    name: "lowestGrade",
+    initial: `enter a number between 1 and 100`,
+    message: `
+What's the lowest grade you want to give?`,
+    validate: lowestGrade => validateNum(lowestGrade),
+  },
+  {
+    type: "number",
+    name: "highestGrade",
+    initial: `enter a number between 1 and 100`,
+    message: `
+What's the Highest grade you want to give?`,
+    validate: highestGrade => validateNum(highestGrade),
+  },
+  {
+    type: "number",
+    name: "skew",
+    initial: `enter a number between 0 and 3`,
+    message: `
+How would you like to skew the grades? a smaller skew, say, .25 will give more higher grades and a higher skew will give more lower grades `,
+    validate: highestGrade => validateNum(highestGrade),
+  },
+]
+
+;(async () => {
+  const response = await prompts(questions)
 
   const results = (function () {
     let finalGradesObj = {}
     for (let i = 1; i <= response.noOfPapers; i++) {
-      finalGradesObj[i] = randn_bm()
+      finalGradesObj[i] = randn_bm(
+        response.lowestGrade,
+        response.highestGrade,
+        1
+      )
     }
     return finalGradesObj
   })()
