2ab64e4234136fc5b8751a362f83b8b7870583df
Don Stevenson <dstevenson@tribalscale.com>
2022-08-04 19:05:35 -0400

    Add skew, lowest and highest mark to grade generator

--- autoGrader.js
+++ autoGrader.js
@@ -1,30 +1,36 @@
-const prompts = require("prompts")
+const prompts = require("prompts");
 
 // helpers
 // **********
 const validateNum = value => {
   if (value > 200 || value < 0 || isNaN(parseFloat(value))) {
-    return "Error!! Please enter a whole number between 0 and 150"
-  } else return true
-}
+    return "Error!! Please enter a whole number between 0 and 150";
+  } else return true;
+};
 
-function randn_bm(min, max, skew) {
+const validateSkew = value => {
+  if (value > 4 || value < 0 || isNaN(value)) {
+    return "Error!! Please enter a number between 0 and 4";
+  } else return true;
+};
+
+const randn_bm = (min, max, skew) => {
   let u = 0,
-    v = 0
-  while (u === 0) u = Math.random() //Converting [0,1) to (0,1)
-  while (v === 0) v = Math.random()
-  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+    v = 0;
+  while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+  while (v === 0) v = Math.random();
+  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
 
-  num = num / 10.0 + 0.5 // Translate to 0 -> 1
-  if (num > 1 || num < 0)
-    num = randn_bm(min, max, skew) // resample between 0 and 1 if out of range
+  num = num / 10.0 + 0.5; // Translate to 0 -> 1
+  if (num > 1 || num < 0) num = randn_bm(min, max, skew);
+  // resample between 0 and 1 if out of range
   else {
-    num = Math.pow(num, skew) // Skew
-    num *= max - min // Stretch to fill range
-    num += min // offset to min
+    num = Math.pow(num, skew); // Skew
+    num *= max - min; // Stretch to fill range
+    num += min; // offset to min
   }
-  return `${num.toFixed(1)}%`
-}
+  return `${num.toFixed(1)}%`;
+};
 
 const questions = [
   {
@@ -50,51 +56,50 @@ to help avoid crying students during office hours.
 
 
 So, how many papers do you need to mark?`,
-    validate: noOfPapers => validateNum(noOfPapers),
+
+    validate: noOfPapers => validateNum(noOfPapers)
   },
   {
     type: "number",
     name: "lowestGrade",
     initial: `enter a number between 1 and 100`,
-    message: `
-What's the lowest grade you want to give?`,
-    validate: lowestGrade => validateNum(lowestGrade),
+    message: `What's the lowest grade you want to give?`,
+    validate: lowestGrade => validateNum(lowestGrade)
   },
   {
     type: "number",
     name: "highestGrade",
     initial: `enter a number between 1 and 100`,
-    message: `
-What's the Highest grade you want to give?`,
-    validate: highestGrade => validateNum(highestGrade),
+    message: `What's the Highest grade you want to give?`,
+    validate: highestGrade => validateNum(highestGrade)
   },
   {
     type: "number",
     name: "skew",
-    initial: `enter a number between 0 and 3`,
-    message: `
-How would you like to skew the grades? a smaller skew, say, .25 will give more higher grades and a higher skew will give more lower grades `,
-    validate: highestGrade => validateNum(highestGrade),
-  },
-]
+    initial: `enter a number between 0 and 4`,
+    message: `How would you like to skew the grades? 
+    a smaller skew, say, 0.25 will give more higher grades, 
+    while a higher skew will give more lower grades`,
+    validate: highestGrade => validateSkew(highestGrade)
+  }
+];
 
-;(async () => {
-  const response = await prompts(questions)
+(async () => {
+  const response = await prompts(questions);
 
-  const results = (function () {
-    let finalGradesObj = {}
+  const results = () => {
+    let finalGradesObj = {};
     for (let i = 1; i <= response.noOfPapers; i++) {
       finalGradesObj[i] = randn_bm(
         response.lowestGrade,
         response.highestGrade,
-        1
-      )
+        response.skew
+      );
     }
-    return finalGradesObj
-  })()
+    return finalGradesObj;
+  };
 
-  console.log(
-    `  
+  const output = `  
     * Thank you for choosing wisely and using AutoGrader! *
     *******************************************************
    
@@ -107,8 +112,8 @@ How would you like to skew the grades? a smaller skew, say, .25 will give more h
     in that case, Autograder *strongly* encourages giving this student
     a grade of 69% 
     Nice. ;)
-        
-    `,
-    results
-  )
-})()
+    
+    ${results()}
+    `;
+  console.log(output);
+})();
