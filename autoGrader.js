8054791951e2189d708d4c8a44755a9a53775743
Don Stevenson <dstevenson@tribalscale.com>
2022-09-06 10:22:15 -0400

    Add random-bm import

--- autoGrader.js
+++ autoGrader.js
@@ -5,25 +5,7 @@ const prompts = require("prompts");
 
 const { validateNum } = require("./helpers/validateNum.js");
 const { validateSkew } = require("./helpers/validateSkew.js");
-
-
-const randn_bm = (min, max, skew) => {
-  let u = 0,
-    v = 0;
-  while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
-  while (v === 0) v = Math.random();
-  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
-
-  num = num / 10.0 + 0.5; // Translate to 0 -> 1
-  if (num > 1 || num < 0) num = randn_bm(min, max, skew);
-  // resample between 0 and 1 if out of range
-  else {
-    num = Math.pow(num, skew); // Skew
-    num *= max - min; // Stretch to fill range
-    num += min; // offset to min
-  }
-  return `${num.toFixed(1)}%`;
-};
+const { randn_bm } = require("./helpers/random_bm.js");
 
 const questions = [
   {
@@ -95,10 +77,7 @@ So, how many papers do you need to mark?`,
     *******************************************************
    
     The list below represents the number and the grade assigned for each paper
-  
-    e.g. 1st paper is notated as 1' and
-    the grade is then given as the corresponding value
-  
+    
     ${results()
       .map((grade, index) => `student no ${index + 1} : ${grade}`)
       .join("\n    ")}
