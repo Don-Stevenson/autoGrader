d6a4b295e5a195758a534fa052ba85b514ff709d
Don Stevenson <dstevenson@tribalscale.com>
2022-09-14 22:26:36 -0400

    Update formatting

--- autoGrader.js
+++ autoGrader.js
@@ -2,14 +2,14 @@ const prompts = require("prompts")
 const { generateGrades } = require("./helpers/generateGrades.js")
 const { questions } = require("./helpers/questions")
 
-(async () => {
-  const response = await prompts(questions);
+;(async () => {
+  const response = await prompts(questions)
 
   const header = `  
   * Thank you for choosing wisely and using AutoGrader! *
   *******************************************************
  
-  The list below represents the number and the grade assigned for each paper`;
+  The list below represents the number and the grade assigned for each paper`
   console.log(
     header,
     "\n\n ",
