4338a3876fe07612d7add34aebb706815b2d9e4b
Don Stevenson <dsteven1@gmail.com>
2022-09-16 02:47:41 -0400

    Update format

--- autoGrader.js
+++ autoGrader.js
@@ -2,19 +2,19 @@ const prompts = require("prompts")
 const { generateGrades } = require("./helpers/generateGrades.js")
 const { questions } = require("./helpers/questions")
 
-;(async () => {
-  const response = await prompts(questions)
+  ; (async () => {
+    const response = await prompts(questions)
 
-  const header = `  
+    const header = `  
   * Thank you for choosing wisely and using AutoGrader! *
   *******************************************************
  
   The list below represents the number and the grade assigned for each paper`
-  console.log(
-    header,
-    "\n\n ",
-    generateGrades(response)
-      .map((grade, index) => `student no ${index + 1} : ${grade}`)
-      .join("\n  ")
-  )
-})()
+    console.log(
+      header,
+      "\n\n ",
+      generateGrades(response)
+        .map((grade, index) => `student no ${index + 1} : ${grade}`)
+        .join("\n  ")
+    )
+  })()
