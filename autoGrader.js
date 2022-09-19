b2407b9f18baf84f515a533426721638c5e381b5
Don Stevenson <dsteven1@gmail.com>
2022-09-19 17:02:14 -0400

    add abort message and logic

--- autoGrader.js
+++ autoGrader.js
@@ -5,16 +5,23 @@ const { questions } = require("./helpers/questions")
   ; (async () => {
     const response = await prompts(questions)
 
+
+
     const header = `  
   * Thank you for choosing wisely and using AutoGrader! *
   *******************************************************
  
   The list below represents the number and the grade assigned for each paper`
-    console.log(
-      header,
-      "\n\n ",
-      generateGrades(response)
-        .map((grade, index) => `student no ${index + 1} : ${grade}`)
-        .join("\n  ")
-    )
+
+    if (!response.noOfPapers || !response.skew || response.lowestGrade || response.highestGrade) console.log(`
+  AutoGrader has been aborted. Goodbye!
+  `)
+    else
+      console.log(
+        header,
+        "\n\n ",
+        generateGrades(response)
+          .map((grade, index) => `student no ${index + 1} : ${grade}`)
+          .join("\n  ")
+      )
   })()
