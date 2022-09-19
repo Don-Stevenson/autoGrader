8f57516d66cb90471a5a98bdac0d145a52cc2a18
Don Stevenson <dsteven1@gmail.com>
2022-09-19 16:52:49 -0400

    Update to if instead of while

--- helpers/random_bm.js
+++ helpers/random_bm.js
@@ -1,8 +1,8 @@
 const randn_bm = (min, max, skew) => {
   let u = 0,
     v = 0
-  while (u === 0) u = Math.random() //Converting [0,1) to (0,1)
-  while (v === 0) v = Math.random()
+  if (u === 0) u = Math.random() //Converting [0,1) to (0,1)
+  if (v === 0) v = Math.random()
   let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
 
   num = num / 10.0 + 0.5 // Translate to 0 -> 1
