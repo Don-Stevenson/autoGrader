a150ef2479e51ca859f563811899b591b1a2f07f
Don Stevenson <dstevenson@tribalscale.com>
2022-10-02 00:09:27 -0400

    destructure response

--- autoGrader.js
+++ autoGrader.js
@@ -3,32 +3,34 @@ const prompts = require("prompts")
 const { generateGrades } = require("./helpers/generateGrades.js")
 const { questions } = require("./helpers/questions")
 
-  // run the program
-  ; (async () => {
-    const response = await prompts(questions)
+// run the program
+;(async () => {
+  const { noOfPapers, skew, lowestGrade, highestGrade } = await prompts(
+    questions
+  )
 
-    const header = `  
+  const header = `  
   * Thank you for choosing wisely and using AutoGrader! *
   *******************************************************
  
   The list below represents the number and the grade assigned for each paper`
 
-    if (
-      !response.noOfPapers ||
-      response.skew < 0 ||
-      response.lowestGrade < 0 ||
-      response.highestGrade > 100 ||
-      response.lowestGrade > response.highestGrade
-    )
-      console.log(`
+  if (
+    !noOfPapers ||
+    skew < 0 ||
+    lowestGrade < 0 ||
+    highestGrade > 100 ||
+    lowestGrade > highestGrade
+  )
+    console.log(`
   AutoGrader has been aborted. Goodbye!
   `)
-    else
-      console.log(
-        header,
-        "\n\n ",
-        generateGrades(response)
-          .map((grade, index) => `student no ${index + 1} : ${grade}`)
-          .join("\n  ")
-      )
-  })()
+  else
+    console.log(
+      header,
+      "\n\n ",
+      generateGrades(lowestGrade, highestGrade, noOfPapers, skew)
+        .map((grade, index) => `student no ${index + 1} : ${grade}`)
+        .join("\n  ")
+    )
+})()
--- helpers/generateGrades.js
+++ helpers/generateGrades.js
@@ -1,6 +1,6 @@
 const { randn_bm } = require("./random_bm.js")
 
-const generateGrades = ({ lowestGrade, highestGrade, noOfPapers, skew }) => {
+const generateGrades = (lowestGrade, highestGrade, noOfPapers, skew) => {
   let finalGradesArr = []
   for (let i = 1; i <= noOfPapers; i++) {
     finalGradesArr.push(randn_bm(lowestGrade, highestGrade, skew))
