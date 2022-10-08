712d599cd0f106488148d8cbdec834dc488d09f0
Don Stevenson <dsteven1@gmail.com>
2022-10-08 11:48:34 -0400

    Add new cases where program can be aborted

--- autoGrader.js
+++ autoGrader.js
@@ -3,34 +3,34 @@ const prompts = require("prompts")
 const { generateGrades } = require("./helpers/generateGrades.js")
 const { questions } = require("./helpers/questions")
 
-// run the program
-;(async () => {
-  const { noOfPapers, skew, lowestGrade, highestGrade } = await prompts(
-    questions
-  )
+  // run the program
+  ; (async () => {
+    const { noOfPapers, skew, lowestGrade, highestGrade } = await prompts(
+      questions
+    )
 
-  const header = `  
+    const header = `  
   * Thank you for choosing wisely and using AutoGrader! *
   *******************************************************
  
   The list below represents the number and the grade assigned for each paper`
 
-  if (
-    !noOfPapers ||
-    skew < 0 ||
-    lowestGrade < 0 ||
-    highestGrade > 100 ||
-    lowestGrade > highestGrade
-  )
-    console.log(`
-  AutoGrader has been aborted. Goodbye!
+    if (
+      !noOfPapers ||
+      skew < 0 ||
+      lowestGrade < 0 ||
+      highestGrade > 100 ||
+      lowestGrade > highestGrade
+    ) console.log(' AutoGrader has been aborted. Goodbye!')
+    else if (!noOfPapers || !skew || !lowestGrade || !highestGrade) console.log(`
+    'a problem has been detected with one or more of the values entered. Please double check your entries and try again.'
   `)
-  else
-    console.log(
-      header,
-      "\n\n ",
-      generateGrades(lowestGrade, highestGrade, noOfPapers, skew)
-        .map((grade, index) => `student no ${index + 1} : ${grade}`)
-        .join("\n  ")
-    )
-})()
+    else
+      console.log(
+        header,
+        "\n\n ",
+        generateGrades(lowestGrade, highestGrade, noOfPapers, skew)
+          .map((grade, index) => `student no ${index + 1} : ${grade}`)
+          .join("\n  ")
+      )
+  })()
